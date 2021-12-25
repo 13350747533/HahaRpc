b19824a4d834edee212e16b61166854454d8e2d5
xiang <x13350747533@163.com>
2021-12-25 21:24:21 +0800

    HahaRpc v1.0.2

---
+++ rpc-server/src/main/java/com/xmj/rpc/server/NettyServer.java
@@ -0,0 +1,102 @@
+package com.xmj.rpc.server;
+
+import com.xmj.rpc.common.bean.RpcRequest;
+import com.xmj.rpc.common.bean.RpcResponse;
+import com.xmj.rpc.common.codec.RpcDecoder;
+import com.xmj.rpc.common.codec.RpcEncoder;
+import com.xmj.rpc.common.util.StringUtil;
+import com.xmj.rpc.registry.ServiceRegistry;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import lombok.SneakyThrows;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class NettyServer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(NettyServer.class);
+
+    private String serviceAddress;
+
+    Map<String, Object> handlerMap = new HashMap<>();
+
+    private ServiceRegistry serviceRegistry;
+
+    public NettyServer(String serviceAddress, ServiceRegistry serviceRegistry) {
+        this.serviceAddress = serviceAddress;
+        this.serviceRegistry = serviceRegistry;
+    }
+
+    public NettyServer(String serviceAddress) {
+        this.serviceAddress = serviceAddress;
+    }
+
+
+    private Thread thread;
+
+
+    public void start() {
+        thread = new Thread(new Runnable() {
+//            ThreadPoolExecutor threadPoolExecutor = ;
+            ExecutorService pool = Executors.newCachedThreadPool();
+
+            @SneakyThrows
+            public void run() {
+                EventLoopGroup bossGroup = new NioEventLoopGroup();
+                EventLoopGroup workerGroup = new NioEventLoopGroup();
+                try{
+                    //创建并初始化Netty服务器 BootStrap 对象
+                    ServerBootstrap bootstrap = new ServerBootstrap();
+                    bootstrap.group(bossGroup, workerGroup);
+                    bootstrap.channel(NioServerSocketChannel.class);
+                    bootstrap.childHandler((new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            ChannelPipeline pipeline = socketChannel.pipeline();
+                            //解码，编码，并处理请求
+                            pipeline.addLast(new RpcDecoder(RpcRequest.class));
+                            pipeline.addLast(new RpcEncoder(RpcResponse.class));
+                            pipeline.addLast(new RpcServerHandler(handlerMap));
+                        }
+                    }));
+                    bootstrap.option(ChannelOption.SO_BACKLOG, 1024);
+                    bootstrap.childOption(ChannelOption.SO_BACKLOG, 1024);
+                    //获取RPC服务器的IP和端口号
+                    String[] addressArray = StringUtil.split(serviceAddress,":");
+                    String ip = addressArray[0];
+                    int port = Integer.parseInt(addressArray[1]);
+                    //启动RPC服务器
+                    ChannelFuture future = bootstrap.bind(ip, port).sync();
+                    //注册RPC服务器
+//                    LOGGER.debug("serviceRegistry is {}", serviceRegistry);
+                    if (serviceRegistry != null) {
+                        LOGGER.debug("1 handleMap is {}", handlerMap);
+                        for(String interfaceName : handlerMap.keySet()){
+                            serviceRegistry.registry(interfaceName, serviceAddress);
+                            LOGGER.debug("registry service: {} => {}", interfaceName, serviceAddress);
+                        }
+                    }
+                    LOGGER.debug("server started on port {}", port);
+                    //关闭 RPC服务器
+                    future.channel().closeFuture().sync();
+                }finally{
+                    workerGroup.shutdownGracefully();
+                    bossGroup.shutdownGracefully();
+                }
+            }
+        });
+
+        thread.start();
+    }
+
+}
--- rpc-server/src/main/java/com/xmj/rpc/server/RpcServer.java
+++ rpc-server/src/main/java/com/xmj/rpc/server/RpcServer.java
@@ -26,69 +26,34 @@ import java.util.Map;
  * 发布RPC服务
  */
 
-public class RpcServer implements ApplicationContextAware, InitializingBean {
+public class RpcServer extends NettyServer implements ApplicationContextAware, InitializingBean {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RpcServer.class);
 
-    private String serviceAddress;
+//    private String serviceAddress;
+//
+//    private ServiceRegistry serviceRegistry;
 
-    private ServiceRegistry serviceRegistry;
 
     /**
      * 存放服务名和服务对象之间的映射关系
      */
-    private Map<String, Object> handlerMap =new HashMap<>();
+//    private Map<String, Object> handlerMap =new HashMap<>();
 
     public RpcServer(String serviceAddress) {
-        this.serviceAddress = serviceAddress;
+        super(serviceAddress);
+//        this.serviceAddress = serviceAddress;
     }
 
     public RpcServer(String serviceAddress, ServiceRegistry serviceRegistry) {
-        this.serviceAddress = serviceAddress;
-        this.serviceRegistry = serviceRegistry;
+        super(serviceAddress, serviceRegistry);
+//        this.serviceAddress = serviceAddress;
+//        this.serviceRegistry = serviceRegistry;
     }
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        EventLoopGroup bossGroup = new NioEventLoopGroup();
-        EventLoopGroup workerGroup = new NioEventLoopGroup();
-        try{
-            //创建并初始化Netty服务器 BootStrap 对象
-            ServerBootstrap bootstrap = new ServerBootstrap();
-            bootstrap.group(bossGroup, workerGroup);
-            bootstrap.channel(NioServerSocketChannel.class);
-            bootstrap.childHandler((new ChannelInitializer<SocketChannel>() {
-                @Override
-                protected void initChannel(SocketChannel socketChannel) throws Exception {
-                    ChannelPipeline pipeline = socketChannel.pipeline();
-                    //解码，编码，并处理请求
-                    pipeline.addLast(new RpcDecoder(RpcRequest.class));
-                    pipeline.addLast(new RpcEncoder(RpcResponse.class));
-                    pipeline.addLast(new RpcServerHandler(handlerMap));
-                }
-            }));
-            bootstrap.option(ChannelOption.SO_BACKLOG, 1024);
-            bootstrap.childOption(ChannelOption.SO_BACKLOG, 1024);
-            //获取RPC服务器的IP和端口号
-            String[] addressArray = StringUtil.split(serviceAddress,":");
-            String ip = addressArray[0];
-            int port = Integer.parseInt(addressArray[1]);
-            //启动RPC服务器
-            ChannelFuture future = bootstrap.bind(ip, port).sync();
-            //注册RPC服务器
-            if (serviceRegistry != null) {
-                for(String interfaceName : handlerMap.keySet()){
-                    serviceRegistry.registry(interfaceName, serviceAddress);
-                    LOGGER.debug("registry service: {} => {}", interfaceName, serviceAddress);
-                }
-            }
-            LOGGER.debug("server started on port {}", port);
-            //关闭 RPC服务器
-            future.channel().closeFuture().sync();
-        }finally{
-            workerGroup.shutdownGracefully();
-            bossGroup.shutdownGracefully();
-        }
+        super.start();
 
     }
 
@@ -105,6 +70,7 @@ public class RpcServer implements ApplicationContextAware, InitializingBean {
                     serviceName += "-" + serviceVersion;
                 }
                 handlerMap.put(serviceName, serviceBean);
+//                LOGGER.debug("handlerMap is {}", handlerMap);
             }
         }
     }
